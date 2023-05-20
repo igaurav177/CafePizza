a435ea31e0dbb4e614746b60aa7be5f65cb8d4a9
Myroslava Shulimenko <mira.shulimenko@gmail.com>
2023-05-20 20:49:05 +0200

    project creation

---
+++ src/main/java/com/example/cafepizza/CafePizzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafepizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafePizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafePizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cafepizza/CafePizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafepizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafePizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
