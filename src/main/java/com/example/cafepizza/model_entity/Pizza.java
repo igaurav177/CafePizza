58b002c62afc8d82fa52fa6d38d4c01ecc8e04f7
Myroslava Shulimenko <mira.shulimenko@gmail.com>
2023-05-23 17:57:00 +0200

    delete files .docx and add pom.xml

--- src/main/java/com/example/cafepizza/model_entity/Pizza.java
+++ src/main/java/com/example/cafepizza/model_entity/Pizza.java
@@ -1,2 +1,25 @@
-package com.example.cafepizza.model_entity;public class Pizza {
+package com.example.cafepizza.model_entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "pizza")
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id_pizza;
+
+
+    public void setId_pizza(String idPizza) {
+        this.id_pizza = idPizza;
+    }
+
+    public String getId_pizza() {
+        return id_pizza;
+    }
 }
